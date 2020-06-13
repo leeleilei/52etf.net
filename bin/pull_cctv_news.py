6d6d459b4973734fe9e5278f8f51e22d92721162
DAVID LEE <leeleilei@gmail.com>
2020-06-13 16:48:52 +0800

    更新新闻脚本

--- bin/pull_cctv_news.py
+++ bin/pull_cctv_news.py
@@ -1,41 +1,12 @@
 import sys
 import jiagu
 
-dest = sys.argv[1]
 
 KEYS = "纳斯达克 中概股 国债 债券 养老 红利 消费 5G 科技 一带一路 稳健 湾区 消费价格 通信 创业板 证券 新能源 汽车 医药 龙头 金融 人工智能 传媒 国企改革 军工 房地产 半导体 原油".split(' ')
 KEYS.extend('航空 海运 交运 保险 公用 农牧 制造 券商 化工 医疗 贸易 安防 建材 房地产 旅游 金属 服务 机械 材料 民航 机场 水泥 水运 汽车 港口 煤炭 物流 环保 电信 电力 电子 石油 船舶 装修 装饰 设备 金属 通讯 酒店 银行 公路'.split(' '))
 KEYS.extend('货币 利率 准备金 央行 国债 股市 房地产 房产 贸易战 关税 原油 局势'.split(' '))
     
-
-#date
-from datetime import datetime
-import pytz
-
-china_tz = pytz.timezone("Asia/Shanghai")
-
-from datetime import datetime, timedelta
-
-try:
-    yesterday_str = sys.argv[2]
-except IndexError:
-    yesterday = datetime.today() - timedelta(days=1)
-    yesterday = china_tz.localize(yesterday)
-    yesterday_str = yesterday.strftime('%Y-%m-%d')
-
-today = datetime.today()
-today = china_tz.localize(today)
-today_str = today.strftime('%Y%m%d')
-updated_str = today.isoformat()
-
-#init tushare
-import tushare as ts
-TOKEN = '2ecdcdc049841ad3c28d13653925f79d41da86fe73dd49f5897f1ec4'
-ts.set_token(TOKEN)
-pro = ts.pro_api()
-news = pro.cctv_news(date=today_str.replace('-',''))
-
-
+#hugo 文章模板
 tpl = '''
 ---
 title: {title}
@@ -46,17 +17,36 @@ draft: false
 {content}
 '''
 
+# 文章保存的相对目录
+dest = sys.argv[1]
+
+# 处理时间
+from datetime import (datetime, timedelta)
+import pytz
+china_tz = pytz.timezone("Asia/Shanghai")
+
+try:
+    today_str = sys.argv[2]
+except IndexError:
+    updated_str = china_tz.localize(datetime.now()).astimezone().isoformat()
+    today_str = china_tz.localize(datetime.now()).astimezone().isoformat()[:10].replace('-','')
+
+#init tushare
+import tushare as ts
+TOKEN = '2ecdcdc049841ad3c28d13653925f79d41da86fe73dd49f5897f1ec4'
+ts.set_token(TOKEN)
+pro = ts.pro_api()
+news = pro.cctv_news(date=today_str.replace('-',''))
+
 
 if not news.empty:
     #title 昨日
     title = 'CCTV新闻联播摘要{date}'.format(date=today_str)
     updated_at = datetime.today().isoformat()
+    
     #content
     import jiagu
-    # reports = []
-    # for text in news.iloc[:,2].to_list():
-    #     reports.append(''.join(jiagu.summarize(text, 1)))
-    # content = '\r\n\r\n'.join(reports)
+
     text = ''.join(news.iloc[:,2].to_list())
     content = '\r\n\r\n'.join(jiagu.summarize(text, 10))
 
@@ -71,8 +61,7 @@ if not news.empty:
         if k in content:
             eco_keywords.append(k)
 
-
-    #黑体强调关键字
+    # 高亮显示关键字，需要hugo默认打开支持html，参考config.yaml
     for k in text_keywords:
         content = content.replace(k, '<span style="border-bottom:4px solid #E32636;">'+k+'</span>')
 
@@ -84,7 +73,7 @@ if not news.empty:
     keywords = ','.join(text_keywords)
 
 
-    # 更新日期是今日
+    # 更新输出内容
     md = tpl.format(
         title=title,
         date=updated_str,
